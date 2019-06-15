3df3bee987fc9d65b5886a0e6ccf5971d4501b48
Kendall Kempton <kemptonk@varentech.com>
2019-06-14 21:04:04 -0400

    Implement employee data API

--- GGProject/GreeterGuru/urls.py
+++ GGProject/GreeterGuru/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('workflow.urls')),
+    path('', admin.site.urls),
     path('workflow/', include('workflow.urls')),
+    re_path('api/(?P<version>(v1|v2))/', include('workflow.urls')),
 ]
