014c065495a05054d4ddd0ed3dea77e30e81c8a6
orndorffc <orndorffc@varentech.com>
2019-06-18 11:56:10 -0700

    Changes administrative functionanality

--- GGProject/GreeterGuru/settings.py
+++ GGProject/GreeterGuru/settings.py
@@ -14,7 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+  os.path.join(PROJECT_PATH, 'templates'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
