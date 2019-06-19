c185eabc17798be747d55310a202e86174be35ab
Kendall Kempton <kemptonk@varentech.com>
2019-06-19 15:08:21 -0400

    Update testfile

--- FaceID/test.py
+++ FaceID/test.py
@@ -15,27 +15,29 @@ def main():
     #emp = requests.get("http://localhost:8000/api/employees/300/")
     #emp = emp.json()
 
+
+    task = {"picture": "cat.jpg", "name": "cat.jpg"}
+    response = requests.post("http://localhost:8000/api/pictures/400/")
+
+    #content = response.json()
+
     """
-    task = {"picture": "cat.jpg"}
-    response = requests.delete("http://localhost:8000/api/pictures/User1_1.jpg/")
-
-    content = response.json()
-
     for person in content:
         for key in person:
             print(key + ":", person[key])
         print()
     """
-    
+    """   
     task = {
-#        "first_name": "Jaylan",
-#        "last_name": "Hall",
+        "first_name": "Jaylan",
+        "last_name": "Hall",
         "email": "hallj@varentech.com",
-        "varen_ID": 400,
+        "emp_ID": 400,
         "keycode": 12345,
     }
 
     response = requests.put("http://localhost:8000/api/employees/450/", json=task)
+    """
     if response:
         print("Success!")
     else:
