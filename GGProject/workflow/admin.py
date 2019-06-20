9fa6f4dc18b71d51989e6a5e14dd94353f7cf093
Kendall Kempton <kemptonk@varentech.com>
2019-06-20 11:30:17 -0400

    Update employee and picture models

--- GGProject/workflow/admin.py
+++ GGProject/workflow/admin.py
@@ -4,42 +4,12 @@ import django.contrib.auth.admin
 from django.db import models
 from django.contrib.auth.models import *
 from django.urls import reverse
+
 # Register your models here.
-
-
-class AdminsPage(admin.ModelAdmin):
-    list_display = ('admin_id', 'admin_email', 'admin_permissions', 'is_two_factor')
-    def save_model(self, request, obj, form, change):
-        obj.Admin = request.Admin
-        super().save_model(request, obj, form, change)
-        print(Admin)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'emp_ID', 'emp_email','emp_permissions','manager_email', 'key_code')
-
-    def save_model(self, request, obj, form, change):
-        obj.Employee = request.Employee
-        super().save_model(request, obj, form, change)
-class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('photo_id', 'badge_photo', 'photos')
-
-    def save_model(self, request, obj, form, change):
-        obj.Photo = request.Photo
-        super().save_model(request, obj, form, change)
-class Temp_PhotoAdmin(admin.ModelAdmin):
-    list_display = ('temp_id','unknown_photo')
-
-    def save_model(self, request, obj, form, change):
-        obj.Temp_Photo = request.Temp_Photo
-        super().save_model(request, obj, form, change)
 admin.site.unregister(Group)
 admin.site.unregister(User)
 
-admin.site.register(Employee, EmployeeAdmin)
-admin.site.register(Admin, AdminsPage)
-admin.site.register(Photo, PhotoAdmin)
-admin.site.register(Temp_Photo, Temp_PhotoAdmin)
-
-AdminsPage.save_as = True
-EmployeeAdmin.save_as = True
-PhotoAdmin.save_as = True
-Temp_PhotoAdmin.save_as = True
+admin.site.register(Employee)
+admin.site.register(Admin)
+admin.site.register(Photo)
+admin.site.register(Temp_Photo)
--- GGProject/workflow/models.py
+++ GGProject/workflow/models.py
@@ -2,36 +2,39 @@ from django.db import models
 
 #authen = models.BooleanField("Two-factor Authentication?", default=0)
 
-# Create your models here.
 class Employee(models.Model):
-    #includes first_name, last_name, emp_ID, emp_email, emp_permissions, manager_email, key_code
     first_name = models.CharField(max_length=30, primary_key = True)
     last_name = models.CharField(max_length=30)
     emp_ID = models.IntegerField(default=0)
-
-    def __str__(self):
-        return("{} {}".format(self.first_name, self.last_name))
-
     emp_email = models.EmailField(max_length = 254, blank = True)
-    permissions_choices = [ #configures roles for different types of employees
-        # 0 can never unlock door
-        # 1 can unlock the door given the alarm is off
-        # 2 can always unlock the door
-        (0,'Never'),
-        (1,'Sometimes'),
-        (2,'Always'),
+    keycode = models.PositiveSmallIntegerField(default = 0)
+    manager_email = models.EmailField(max_length = 254, default = "")
+    
+    #configures roles for different types of employees
+    permissions_choices = [ 
+        (0,'Never'),     # 0 can never unlock door
+        (1,'Sometimes'), # 1 can unlock the door given the alarm is off
+        (2,'Always'),    # 2 can always unlock the door
     ]
     emp_permissions = models.CharField(
         max_length = 15,
         choices = permissions_choices,
         default = 0
     )
-    #Manager Information
-    manager_email = models.EmailField(max_length = 254, default = "")
-    #used only for two-factor authentication
-    key_code = models.PositiveSmallIntegerField(default = 0)
 
-#class Admin(models.Model):
+    def __str__(self):
+        return("{} {}".format(self.first_name, self.last_name))
+
+
+class Picture(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True)
+    picture = models.ImageField(null=True)
+    name = models.CharField(null=True, unique=True, max_length=30)
+    
+    def __str__(self):
+        return("{}".format(self.name))
+    
+
 class Admin(models.Model):
     #includes admin_id, admin_email, admin_permissions, is_two_factor
     admin_id = models.IntegerField(primary_key = True)
@@ -48,18 +51,9 @@ class Admin(models.Model):
     )
     is_two_factor = models.BooleanField("Two-Factor Authentication", default = False)
 
-#Save recognized photos
-class Photo(models.Model):
-    photo_id = models.ForeignKey(Employee, on_delete = models.CASCADE, related_name = "+")
-    def __str__(self):
-        return("{}".format(self.photo_id))
-    badge_photo = models.ImageField()
-    #sets name of photo file
-    def user_directory_path(instance, filename):
-        return '{0}_{1}'.format(instance.Employee.emp_ID, photo_id)
-    photos = models.FileField(upload_to = user_directory_path)
-    #sends photos to MEDIA_ROOT set in GreeterGuru/GGProject/GreeterGuru/settings.py
 
+
+    
 #Save unknown photos
 class Temp_Photo(models.Model):
     temp_id = models.AutoField(max_length = 100, primary_key = True)
