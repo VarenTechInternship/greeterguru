b58ffd2c24ce5f121dcb5d72399f603835b07f66
orndorffc <orndorffc@varentech.com>
2019-06-20 06:04:32 -0700

    Start testing database

--- GGProject/workflow/urls.py
+++ GGProject/workflow/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('employees/', views.ListEmployeesView.as_view(), name="employees-all"),
     #path('employees/<int:pk>/', views.SingleEmployeeView, name="employee-one"),
+    path('admin/', views.ListAdminsView.as_view(), name="admins-all"),
+    path('photo/', views.ListPhotosView.as_view(), name="photos-all"),
+    path('temp-photo/', views.ListTempPhotosView.as_view(), name="temp-photos-all"),
+
 ]
--- GGProject/workflow/views.py
+++ GGProject/workflow/views.py
@@ -1,9 +1,21 @@
 from rest_framework import generics
-from .models import Employee, Admin
-from .serializers import EmployeesSerializer
+from .models import Employee, Admin, Photo, Temp_Photo
+from .serializers import EmployeesSerializer, AdminsSerializer, PhotosSerializer, TempPhotosSerializer
 
 # Displays all Employees' attributes
 class ListEmployeesView(generics.ListAPIView):
     # Set of attributes to query
     queryset = Employee.objects.all()
     serializer_class = EmployeesSerializer
+class ListAdminsView(generics.ListAPIView):
+    # Set of attributes to query
+    queryset = Admin.objects.all()
+    serializer_class = AdminsSerializer
+class ListPhotosView(generics.ListAPIView):
+    # Set of attributes to query
+    queryset = Photo.objects.all()
+    serializer_class = PhotosSerializer
+class ListTempPhotosView(generics.ListAPIView):
+    # Set of attributes to query
+    queryset = Temp_Photo.objects.all()
+    serializer_class = TempPhotosSerializer
