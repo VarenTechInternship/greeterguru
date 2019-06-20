68e787e0384e0da05e8ae872b80b63f2de0449bf
orndorffc <orndorffc@varentech.com>
2019-06-20 11:14:26 -0700

    Removed some default functionality and add dummy model

--- GGProject/workflow/models.py
+++ GGProject/workflow/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class emp (models.Model):
+    first=models.CharField(null = True, max_length = 10)
+    last=models.CharField(null=True, max_length = 10)
