6e4e5a54a72a6dcdb39d81c7d0332c2391824e24
orndorffc <orndorffc@varentech.com>
2019-06-21 08:26:49 -0700

    Add dummy models

--- GGProject/workflow/admin.py
+++ GGProject/workflow/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import site
 from django.contrib.auth.models import User, Group
-
-from django.db import models
+from django.contrib.auth.admin import UserAdmin
 import django.contrib.auth.admin
 from .models import *
+from . import views
 
 admin.autodiscover()
 
-admin.site.register(emp)
-admin.site.unregister(User)
+#admin.site.register(emp)
 admin.site.unregister(Group)
--- GGProject/workflow/models.py
+++ GGProject/workflow/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class emp (models.Model):
-    first=models.CharField(null = True, max_length = 10)
-    last=models.CharField(null=True, max_length = 10)
