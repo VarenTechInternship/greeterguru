13a3794e60824ab7e3866604b2b0f873698fdbaf
orndorffc <orndorffc@varentech.com>
2019-06-21 08:26:49 -0700

    Add dummy models

--- GGProject/workflow/views.py
+++ GGProject/workflow/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+
+def send(request):
+    emp1 = User(username = 'orndorffc',first_name = "caroline", last_name = "orndorff", email = "orndorffc@varentech.com")
+    emp1.save()
+    name = emp1.get_full_name(self)
+    print(name)
+    return render(request)
