d3949d28db94585caf3d41cbb44296132974204a
orndorffc <orndorffc@varentech.com>
2019-06-25 07:22:22 -0700

    Resolve merge conflicts

--- GGProject/workflow/tests.py
+++ GGProject/workflow/tests.py
@@ -1,8 +1,4 @@
 from django.test import TestCase
-<<<<<<< HEAD
-
-# Create your tests here.
-=======
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
@@ -12,4 +8,3 @@ from . import views
 
 
 # Place your tests here
->>>>>>> 33334a727aab38b2014962cb1609515e70ddc8b9
