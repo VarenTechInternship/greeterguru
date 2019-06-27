8eec4c2eb9fe726c2e7f1cd52e007626ce467f94
orndorffc <orndorffc@varentech.com>
2019-06-27 07:36:11 -0700

    Create Models tests for Employee, Picture, and TempPhoto

--- GGProject/workflow/tests/test_admin.py
+++ GGProject/workflow/tests/test_admin.py
@@ -1,9 +1,11 @@
 from django.test import TestCase, Client
 import unittest
-
+import requests
+from django.contrib.auth.models import User
 #test pages exist
-class SimpleTest(unittest.TestCase):
+class TestAdmin(unittest.TestCase):
     client = Client()
+    
     def test_admin(self): #admin main exists
         response = self.client.get('/')
         self.assertEqual(response.status_code, 302)
--- GGProject/workflow/urls.py
+++ GGProject/workflow/urls.py
@@ -4,6 +4,7 @@ import rest_framework
 from rest_framework.authtoken import views as authviews
 
 urlpatterns = [
+<<<<<<< HEAD
     path('employees/', views.ListEmployees.as_view()),
     path('employees/<int:emp_ID>/', views.SingleEmployee.as_view()),
     path('pictures/', views.ListPictures.as_view()),
@@ -11,4 +12,13 @@ urlpatterns = [
     path('pictures/<str:name>/', views.SinglePicture.as_view()),
     path('temp-photos/', views.ListTempPhotos.as_view()),
     path('token-auth/', rest_framework.authtoken.views.obtain_auth_token),
+=======
+    path('employees/', views.ListEmployees.as_view(), name = 'employees'),
+    path('employees/<int:emp_ID>/', views.SingleEmployee.as_view(), name = 'singleemployee'),
+    path('pictures/', views.ListPictures.as_view(), name = 'allpictures'),
+    path('pictures/<int:emp_ID>/', views.EmployeePictures.as_view(), name = 'allemployeepictures'),
+    path('pictures/<str:name>/', views.SinglePicture.as_view(), name = 'singleemployeepicture'),
+    path('temp-photos/', views.ListTempPhotos.as_view(), name = 'alltempphotos'),
+    path('token-auth/', authviews.obtain_auth_token),
+>>>>>>> 8621589... Create Models tests for Employee, Picture, and TempPhoto
 ]
