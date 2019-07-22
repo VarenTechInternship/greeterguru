7f346581218a01735a8d3a60fcb2b114855c704c
root <kemptonk@varentech.com>
2019-07-22 11:26:35 -0700

    Add settings that allow employees to log onto the site with their AD credentials

--- bin/install-1.sh
+++ bin/install-1.sh
@@ -8,7 +8,7 @@ sudo apt-get -y upgrade
 
 echo "
 INSTALLING NECESSARY PACKAGES:"
-sudo apt-get install -y build-essential cmake unzip pkg-config libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libatlas-base-dev gfortran python3-dev python3-testresources
+sudo apt-get install -y build-essential cmake unzip pkg-config libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libatlas-base-dev gfortran python3-dev python3-testresources python-dev libldap2-dev libsasl2-dev libssl-dev
 
 echo "
 RETRIEVING OPENCV PACKAGES:"
